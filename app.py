98068f24eb3aed1b2a10be5d768628a987261fb4
jovanniochoa <57188689+jovanniochoa@users.noreply.github.com>
2023-05-23 17:03:40 -0500

    Imported Flask for backend

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-from flask import Flask, jsonify, render_template
+import os
+import pickle
+import importlib.util
+import logging
+import librosa
+import numpy as np
+from flask import Flask, jsonify, render_template, redirect, url_for, request
+
+# Check if scikit-misc package is installed, and install it if not
+package_name = 'scikit-misc'
+spec = importlib.util.find_spec(package_name)
+if spec is None:
+    os.system(f"pip install scikit-misc")
 
 app = Flask(__name__)
 
@@ -6,23 +18,55 @@ app = Flask(__name__)
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get the input data from the request
-    data = request.json
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+        file = request.files['file']  # Access the uploaded file
+        
+        # Save the file to a designated location on the server
+        file_path = os.path.join('uploads', file.filename)
+        file.save(file_path)
+        
+        # Process the uploaded file with the machine learning model
+        result = process_file(file_path)
+        
+        return jsonify({'message': 'File uploaded successfully', 'result': result})
+    except Exception as e:
+        app.logger.error(str(e))
+        return jsonify({'message': 'Error occurred while uploading file'})
 
-    # Preprocess the input data if needed
-    # ...
+def process_file(file_path):
+    # Preprocess the audio file
+    signal, sr = librosa.load(file_path, sr=22050)
+    
+    # Extract features
+    # Placeholder code, replace with your feature extraction code
+    feature = extract_features(signal, sr)
+    
+    # Reshape the feature for model prediction
+    feature = feature.reshape(1, -1)
+    
+    # Perform model prediction
+    predicted_class = model.predict(feature)[0]
+    
+    return predicted_class
 
-    # Make predictions using the loaded model
-    predictions = model.predict(data)
 
-    # Return the predictions as JSON response
-    return jsonify(predictions.tolist())
+def extract_features(signal, sr):
+    # Placeholder code for feature extraction
+    # Replace with your actual feature extraction code
+    # Here's an example using Mel-Frequency Cepstral Coefficients (MFCC)
+    mfccs = librosa.feature.mfcc(signal, sr=sr, n_mfcc=13)
+    feature = np.mean(mfccs.T, axis=0)  # Take the mean along the time axis
+    
+    return feature
 
-@app.route('/scatter', methods=['GET'])
-def scatter():
-    return render_template('scatter.html')
+@app.route('/')
+def index():
+    return render_template('MusicClassification.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.logger.setLevel(logging.ERROR)
+    handler = logging.StreamHandler()
+    app.logger.addHandler(handler)
+    app.run(debug=True)
